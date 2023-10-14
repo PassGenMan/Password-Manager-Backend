6e0371e8125aed0edd531814be9c12c52d8f58ec
nazmus-saqib008 <iamnazmussaqib@outlook.com>
2023-10-14 16:50:39 +0530

    cors open to all

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const Data= require('./models/Data')
 mongoose.connect(process.env.URI)
 
 const app= express();
-app.use(cors({credentials:true, origin: url, allowedHeaders: ['Content-Type'], methods: 'GET,POST,PUT,DELETE'}));
+app.use(cors({credentials:true, origin: "*" , allowedHeaders: ['Content-Type'], methods: 'GET,POST,PUT,DELETE'}));
 app.use(express.json());
 app.use(cookieParser());
 
