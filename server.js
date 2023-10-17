617816d46e2dc71e158ca8178e8fc9852e526432
nazmus-saqib008 <iamnazmussaqib@outlook.com>
2023-10-17 10:50:10 +0530

    get profile optimised

--- server.js
+++ server.js
@@ -74,10 +74,14 @@ app.post('/login', async (req, res)=>{
 
 app.get('/profile', (req, res)=>{
     const {token}= req.cookies;
-    jwt.verify(token, secret, {}, (err, info)=>{
-        if (err) throw err;
-        res.json(info);
-    })
+    if(token){
+        jwt.verify(token, secret, {}, (err, info)=>{
+            if (err) throw err;
+            res.json(info);
+        })
+    }else{
+        res.json(null);
+    }
 })
 
 app.post('/logout', (req,res)=>{
