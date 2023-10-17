7d59e7d413b3d91f3dd026c1f9aa6eca8d36401c
nazmus-saqib008 <iamnazmussaqib@outlook.com>
2023-10-17 11:41:25 +0530

    testing render site

--- frontendURL.js
+++ frontendURL.js
@@ -1,4 +1,5 @@
-const url= 'https://password-manager-hacktoberfest.vercel.app';
+// const url= 'https://password-manager-hacktoberfest.vercel.app';
 // const url= 'http://localhost:3000';
+const url= 'https://passmanclient.onrender.com';
 
 module.exports= url;
