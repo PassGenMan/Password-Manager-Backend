c7a842d359d91baf43f5bfb251b899bf83bc497f
nazmus-saqib008 <iamnazmussaqib@outlook.com>
2023-11-30 12:28:02 +0530

    frontend url reset

--- frontendURL.js
+++ frontendURL.js
@@ -1,5 +1,6 @@
-const url= 'https://password-manager-hacktoberfest.vercel.app';
+// const url= 'https://password-manager-hacktoberfest.vercel.app';
 // const url= 'http://localhost:3000';
 // const url= 'https://passmanclient.onrender.com';
+const url = 'https://password-manager-nazmus-saqib008.vercel.app/'
 
 module.exports= url;
